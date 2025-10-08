106aa5d9706de192213ce4b517e8e02e29925eea
GFFloyd <gfaracovip@gmail.com>
2025-10-08 12:11:16 -0300

    adicionado lógica para adicionar artista ao JSON

--- Program.cs
+++ Program.cs
@@ -1,23 +1,40 @@
 ﻿using System.Text.Json;
-var jsonPath = "test.json";
-var teste = new Artista(
-    "Brad Pitt",
-    "homem",
-    "ator",
-    "estadunidense",
-    55,
-    false,
-    1.70f,
-    "",
-    "branco",
-    ["Globo de Ouro", "BAFTA"]
-    );
-
-// Console.WriteLine(teste);
-//to-do: abrir o JSON e escrever o objeto serializado ao final do arquivo
-var options = new JsonSerializerOptions { WriteIndented = true };
-var serialized = JsonSerializer.Serialize(teste);
-using StreamWriter sw = File.AppendText(jsonPath);
-// Console.WriteLine(sw.ReadToEnd());
-sw.WriteLine(serialized, options);
-sw.Close();
+using System.Text.Json.Nodes;
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        const string jsonPath = "/home/gfaraco/termo/test.json";
+        var teste = new Artista(
+            "Brad Pitt",
+            "homem",
+            "ator",
+            "estadunidense",
+            55,
+            false,
+            1.70f,
+            "",
+            "branco",
+            ["Globo de Ouro", "BAFTA"]
+            );
+        Console.WriteLine(Path.GetFullPath(jsonPath));
+        AdicionarArtistaAoJSON(jsonPath, teste);
+    }
+    public static void AdicionarArtistaAoJSON(string path, Artista artist)
+    {
+        var options = new JsonSerializerOptions { WriteIndented = true, PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
+        JsonArray jsonArray;
+    
+        if (File.Exists(path) && new FileInfo(path).Length > 0)
+        {
+            var jsonAtual = File.ReadAllText(path);
+            jsonArray = JsonNode.Parse(jsonAtual) as JsonArray ?? [];
+        }
+        else
+            jsonArray = [];
+        var artistaNode = JsonSerializer.SerializeToNode(artist, options);
+        jsonArray.Add(artistaNode);
+        
+        File.WriteAllText(path, jsonArray.ToJsonString(options));
+    }
+}
